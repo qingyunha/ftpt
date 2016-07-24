0807ce7df3bda0ad4e7fa4d138ab7edf8dd1673f
qingyun <845767657@qq.com>
2016-07-24 20:31:27 +0800

    Init commit

---
+++ ftpt.py
@@ -0,0 +1,42 @@
+import os
+from flask_autoindex import AutoIndex
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'gz'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+AutoIndex(app, browse_root=UPLOAD_FOLDER)
+
+def allowed_file(filename):
+    return '.' in filename and \
+                    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route("/upload/", methods=["POST"])
+@app.route("/upload/<path:path>", methods=["POST"])
+def upload(path=""):
+    print "upload path is ", path
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        print('no file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        print('No selected file')
+        return redirect("/" + path)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'],
+            path, filename))
+        return redirect("/" + path)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
