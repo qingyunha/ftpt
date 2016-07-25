fe8f13a42b3d9b9acb5e18b4e80405dcece75b9d
taoqy <taoqy@51idc.com>
2016-07-25 14:32:21 +0800

    secure_filename new_floder

--- ftpt.py
+++ ftpt.py
@@ -4,47 +4,38 @@ from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'gz'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 AutoIndex(app, browse_root=UPLOAD_FOLDER)
-
-def allowed_file(filename):
-    return '.' in filename and \
-                    filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
 @app.route("/upload/", methods=["POST"])
 @app.route("/upload/<path:path>", methods=["POST"])
 def upload(path=""):
-    print "upload path is ", path
     # check if the post request has the file part
     if 'file' not in request.files:
-        print('no file part')
-        return redirect(request.url)
-    file = request.files['file']
-    # if user does not select file, browser also
-    # submit a empty part without filename
-    if file.filename == '':
-        print('No selected file')
         return redirect("/" + path)
-    if file and allowed_file(file.filename):
+    file = request.files['file']
+    if file:
         filename = secure_filename(file.filename)
+        if not filename:
+            return redirect("/" + path)
         file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'],
             path, filename))
-        return redirect("/" + path)
+    return redirect("/" + path)
 
 
 @app.route("/newfolder/", methods=["POST"])
 @app.route("/newfolder/<path:path>", methods=["POST"])
 def new_folder(path=""):
-    folder = request.form["name"]
-    os.mkdir(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], path,
-        folder))
+    # there still a risk: path (since user can change this)
+    folder = secure_filename(request.form["name"])
+    abs_path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], path, folder)
+    if not os.path.exists(abs_path):
+        os.mkdir(abs_path)
     return redirect("/" + path)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
